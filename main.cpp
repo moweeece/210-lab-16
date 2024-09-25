345772bdcf50970cc5257d401cdb7cc5d96b14f0
moweeece <mjespinosa990@gmail.com>
2024-09-25 14:34:05 -0700

    2nd commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
+#include <iomanip>
 using namespace std;
+
+// Class Declaration
+class Color 
+{
+    private:
+    int red;
+    int green;
+    int blue;
+
+    public:
+    // getters and setters
+    int getRed()          { return red; }
+    void setRed(int r)    { red = r; }
+    int getGreen()        { return green; }
+    void setGreen(int g)  { green = g; }
+    int getBlue()         { return blue; }
+    void setBlue(int b)   { blue = b; }
+
+    // print method
+    void print() {
+        cout << setw(15) << "Red: " << red << endl;
+        cout << setw(15) << "Green: " << green << endl;
+        cout << setw(15) << "Blue: " << blue << endl;
+    }
+
+};
+
 
 int main() {
 
